2f5c191b78fd7ae22ad7c9f94bfcf06dc1ce622f
er-raoniz <rahulvsoni1997@gmail.com>
2020-11-10 15:56:30 +0530

    add item based recommender

--- src/MovieRecommender.java
+++ src/MovieRecommender.java
@@ -1,12 +1,16 @@
 import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.EuclideanDistanceSimilarity;
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.Recommender;
 import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 import java.io.File;
@@ -19,37 +23,53 @@ public class MovieRecommender {
         try {
             //creating data model
             DataModel datamodel = new FileDataModel(new File("data/moviedata.csv")); //data
+
+            // User-based Recommender
             File user_file = new File("data/user-based.txt");
             user_file.createNewFile();
             FileWriter user_writer = new FileWriter(user_file);
 
+            // Creating UserSimilarity object.
+            UserSimilarity usersimilarity = new PearsonCorrelationSimilarity(datamodel);
+            // User Neighbourhood object.
+            UserNeighborhood userneighborhood = new ThresholdUserNeighborhood(0.1, usersimilarity, datamodel);
+            // User Recommender
+            UserBasedRecommender userRecommender = new GenericUserBasedRecommender(datamodel, userneighborhood, usersimilarity);
+
+            // Item-based Recommender
             File item_file = new File("data/item-based.txt");
             item_file.createNewFile();
-            FileWriter item_wirter = new FileWriter(item_file);
+            FileWriter item_writer = new FileWriter(item_file);
 
-            //Creating UserSimilarity object.
-            UserSimilarity usersimilarity = new PearsonCorrelationSimilarity(datamodel);
-
-            //UserNeighbourHHood object.
-            UserNeighborhood userneighborhood = new ThresholdUserNeighborhood(0.1, usersimilarity, datamodel);
-
-            //UserRecommender
-            UserBasedRecommender recommender = new GenericUserBasedRecommender(datamodel, userneighborhood, usersimilarity);
+            // Creating item similarity object
+            ItemSimilarity itemSimilarity = new EuclideanDistanceSimilarity(datamodel);
+            // Item Recommender
+            Recommender itemRecommender = new GenericItemBasedRecommender(datamodel, itemSimilarity);
 
             //Write recommendations to file
             for (LongPrimitiveIterator users = datamodel.getUserIDs(); users.hasNext(); ) {
                 long userId = users.next();
-                List<RecommendedItem> recommendations = recommender.recommend(userId, 10);
-                StringBuilder writeToFile = new StringBuilder(userId + "[");
-                for (RecommendedItem recommendation : recommendations) {
-                    writeToFile.append(recommendation.getItemID()).append(":").append(recommendation.getValue()).append(",");
+                List<RecommendedItem> userRecommendations = userRecommender.recommend(userId, 10);
+                StringBuilder writeToUserFile = new StringBuilder(userId + "[");
+                for (RecommendedItem recommendation : userRecommendations) {
+                    writeToUserFile.append(recommendation.getItemID()).append(":").append(recommendation.getValue()).append(",");
                 }
-                writeToFile.append("]").append("\n");
-                user_writer.write(writeToFile.toString());
+                writeToUserFile.append("]").append("\n");
+                user_writer.write(writeToUserFile.toString());
 
+                List<RecommendedItem> itemRecommendations = itemRecommender.recommend(userId, 10);
+                StringBuilder writeToItemFile = new StringBuilder(userId + "[");
+                for (RecommendedItem recommendation : itemRecommendations) {
+                    writeToItemFile.append(recommendation.getItemID()).append(":").append(recommendation.getValue()).append(",");
+                }
+                writeToItemFile.append("]").append("\n");
+                item_writer.write(writeToItemFile.toString());
             }
             user_writer.flush();
             user_writer.close();
+
+            item_writer.flush();
+            item_writer.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
